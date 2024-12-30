e2b57f9d1ecae5659231405815bb737ea7ced6dc
stepan <stepanlezennikov@gmail.com>
2024-12-30 17:26:16 +0300

    removed unnecessary comment

--- middleware/token_middleware.py
+++ middleware/token_middleware.py
@@ -3,17 +3,5 @@ from django.conf import settings
 
 class TokenMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        # auth_header = request.META.get('HTTP_AUTHORIZATION', '')
-        # if auth_header.startswith('Bearer '):
-        #     token = auth_header[7:]  # Убираем "Bearer "
-        #     try:
-        #         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        #         request.user_data = payload
-        #     except jwt.ExpiredSignatureError:
-        #         request.user_data = None
-        #     except jwt.InvalidTokenError:
-        #         request.user_data = None
-        # else:
-        #     request.user_data = None
         request.user_id = 1
         request.user_email = 'stepanlezennikov@gmail.com'
